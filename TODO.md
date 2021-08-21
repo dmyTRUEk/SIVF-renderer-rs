bcf331015577a8bfa796f1fc216602b816172f99
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2021-08-21 20:05:07 +0300

    added TODO, setting everything up

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO:
+
+- `cargo run examples_old/file_example_1.sivf` must print file content
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+//! This is main file
+
+use std::env;
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let args_all: Vec<String> = env::args().collect();
+    println!("args_all = {:?}", args_all);
+
+    let args: Vec<String> = (&args_all[1..]).to_vec();
+    println!("args = {:?}", args);
 }
